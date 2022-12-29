6db991f2238120a8529fbc6086a54a11e80f7bea
sanial <sanial369@gmail.com>
2022-12-29 04:41:17 -0500

    Riverpod TrainView data async instead of flutters

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,31 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:septa_commuter_app/dataprovider/data_provider.dart';
 import 'package:septa_commuter_app/models/train_view/train_view.dart';
 
-//Fetch JSON document using http.get() method
-Future<List<TrainView>> fetchTrainViews(http.Client client) async {
-  final response =
-      await client.get(Uri.parse('http://www3.septa.org/hackathon/TrainView/'));
+// //Fetch JSON document using http.get() method
+// Future<List<TrainView>> fetchTrainViews(http.Client client) async {
+//   final response =
+//       await client.get(Uri.parse('http://www3.septa.org/hackathon/TrainView/'));
 
-  if (response.statusCode == 200) {
-    print('200 Works');
-  } else {
-    throw Exception('Failed to load album');
-  }
+//   if (response.statusCode == 200) {
+//     print('200 Works');
+//   } else {
+//     throw Exception('Failed to load album');
+//   }
 
-  //Use the compute function to run parseTrainViews in a separate isolate
-  return compute(parseTrainViews, response.body);
-}
+//   //Use the compute function to run parseTrainViews in a separate isolate
+//   return compute(parseTrainViews, response.body);
+// }
 
-//Funct that convertes a response body into a List<TrainView>
-List<TrainView> parseTrainViews(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<TrainView>((json) => TrainView.fromJson(json)).toList();
-}
+// //Funct that convertes a response body into a List<TrainView>
+// List<TrainView> parseTrainViews(String responseBody) {
+//   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+//   return parsed.map<TrainView>((json) => TrainView.fromJson(json)).toList();
+// }
 
-void main() => runApp(const MyApp());
+void main() => runApp(ProviderScope(child: const MyApp(),));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -46,61 +47,92 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context, ref) {
+    final _trainview_data = ref.watch(userDataProvider);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FetchTrainView'),
+        ),
+        body: _trainview_data.when(
+          data: (_trainview_data) {
+            List<TrainView> trainviewList = _trainview_data
+                .map(
+                  (e) => e,
+                )
+                .toList();
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: trainviewList.length,
+                      itemBuilder: (context, index) {
+                        return Chip(label: Text(trainviewList[index].line!));
+                      }),
+                )
+              ],
+            );
+          },
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: (() => const Center(
+                child: CircularProgressIndicator(),
+              )),
+        ));
+  }
+  //State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<List<TrainView>>(
-          future: fetchTrainViews(http.Client()),
-          builder: ((context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('$snapshot!.error');
-              // const Center(
-              //   child: Text("An error has occurred!"),
-              // );
-            } else if (snapshot.hasData && snapshot.data!.isEmpty) {
-              // got data from snapshot but it is empty
-              return Text("no data");
-            } else if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: ((context, index) {
-                    return Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          color: Colors.amber[600],
-                          width: 5,
-                          height: 48,
-                        ),
-                        Chip(
-                          label: Text(snapshot.data![index].line!),
-                        ),
-                        Container(
-                          height: 50,
-                          color: Colors.white10,
-                          child: Text(snapshot.data![index].trainno!),
-                        ),
-                      ],
-                    );
-                    // ListTile(
-                    //   title: Text(snapshot.data![index].line),
-                    //   subtitle: Text(snapshot.data![index].dest),
-                    // );
-                  }));
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          })),
-    );
-  }
-}
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: FutureBuilder<List<TrainView>>(
+//           future: fetchTrainViews(http.Client()),
+//           builder: ((context, snapshot) {
+//             if (snapshot.hasError) {
+//               return Text('$snapshot!.error');
+//               // const Center(
+//               //   child: Text("An error has occurred!"),
+//               // );
+//             } else if (snapshot.hasData && snapshot.data!.isEmpty) {
+//               // got data from snapshot but it is empty
+//               return Text("no data");
+//             } else if (snapshot.hasData) {
+//               return ListView.builder(
+//                   itemCount: snapshot.data!.length,
+//                   itemBuilder: ((context, index) {
+//                     return Row(
+//                       children: [
+//                         Container(
+//                           margin: const EdgeInsets.all(10),
+//                           color: Colors.amber[600],
+//                           width: 5,
+//                           height: 48,
+//                         ),
+//                         Chip(
+//                           label: Text(snapshot.data![index].line!),
+//                         ),
+//                         Container(
+//                           height: 50,
+//                           color: Colors.white10,
+//                           child: Text(snapshot.data![index].trainno!),
+//                         ),
+//                       ],
+//                     );
+//                     // ListTile(
+//                     //   title: Text(snapshot.data![index].line),
+//                     //   subtitle: Text(snapshot.data![index].dest),
+//                     // );
+//                   }));
+//             } else {
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }
+//           })),
+//     );
+//   }
+// }
